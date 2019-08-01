88239fd7256731a98f5be90609bab16488ffd01a
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-08-01 16:15:10 -0300

    learning how to iterate over arrays

--- 06-lists/for-forin/service.js
+++ 06-lists/for-forin/service.js
@@ -7,9 +7,6 @@ async function obterPessoas(nome) {
     return response.data;
 }
 
-obterPessoas('r2').then(function(resultado) {
-    console.log('Resultado', resultado);
-})
-.catch(function(error) {
-    console.error('Deu ruim', error);
-})
+module.export =  {
+    obterPessoas
+}
